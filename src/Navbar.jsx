545d9fbb1082dcd027a568ed0bf89ef79eeb1b11
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-10-26 22:43:41 +0500

    Add navlink

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return(
@@ -9,7 +10,7 @@ const Navbar = () => {
 
              
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Talha Technicals</a>
+  <NavLink className="navbar-brand" to="/">Talha Technicals</NavLink>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -17,16 +18,16 @@ const Navbar = () => {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <NavLink className="nav-link" to="/">Home <span className="sr-only">(current)</span></NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Services</a>
+        <NavLink activeClassName="menu_active" className="nav-link" to="/service">Services</NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">About</a>
+        <NavLink activeClassName="menu_active" className="nav-link" to="/about">About</NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Contact</a>
+        <NavLink activeClassName="menu_active" className="nav-link" to="/contact">Contact</NavLink>
       </li>
     </ul>
   </div>
